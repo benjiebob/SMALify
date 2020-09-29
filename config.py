253a4b3a18588b83596496851d8731069f4af98d
Benjamin Biggs <benjbiggs@outlook.com>
2020-09-29 18:08:39 -0400

    checked the config defaults

--- config.py
+++ config.py
@@ -27,8 +27,6 @@ WINDOW_SIZE = 10 # Changed number of frames processed in one go.
 CHECKPOINT_NAME = "CHECKPOINT_TO_RUN"
 EPOCH_NAME = "st10_ep0" # convention used for the final output file. Don't change this without good reason.
 
-
-
 # SMAL
 SMAL_FILE = join(data_path, 'smal', 'my_smpl_00781_4_all.pkl')
 SMAL_DATA_FILE = join(data_path, 'smal', 'my_smpl_data_00781_4_all.pkl')
